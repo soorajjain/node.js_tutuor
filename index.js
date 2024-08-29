5f6c5973327ea18060db15fce1cfd97a08ec2b16
soorajain <soorajjain51@gmail.com>
2024-08-29 11:08:08 +0530

    OS : trying some of the built in os methods

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const object = require("./object");
+const os = require("os");
 
-console.log(object.age);
+console.log(object);
+console.log(os.freemem());
+console.log(os.hostname());
+// console.log(exports, require, module, __filename, __dirname);
--- object.js
+++ object.js
@@ -1,5 +1,10 @@
+// (function (exports, require, module, __filename, __dirname) {
+//module wrapper no neeed to write mannulay nodejs handle these automatically
+
 const object = {
   name: "sooraj",
   age: 22,
 };
+// console.log(exports, require, module, __filename, __dirname);
 module.exports = object;
+// });
